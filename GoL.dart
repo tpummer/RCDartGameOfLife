9c1cff23704edf18366c4ffcdcc386794f0bc27a
Peter Kofler <peter.kofler@code-cop.org>
2012-05-31 00:27:50 +0200

    format, whitespace, add links to readme

--- GoL.dart
+++ GoL.dart
@@ -1,29 +1,31 @@
 #import('../../../dart-sdk/lib/unittest/unittest.dart');
 
+// see http://rosettacode.org/wiki/Conway%27s_Game_of_Life
+
 class State {
   const State(this.symbol);
 
-  static final ALIVE = const State('X');
-  static final DEAD = const State('_');
-  
+  static final ALIVE = const State('#');
+  static final DEAD = const State(' ');
+
   final String symbol;
 }
 
 class Rule {
   Rule(this.cellState);
-  
+
   reactToNeighbours(int neighbours){
-    if(neighbours < 2 || neighbours > 3) {
-      cellState = State.DEAD;
-    } else if (neighbours == 3) {
+    if (neighbours == 3) {
       cellState = State.ALIVE;
+    } else if (neighbours != 2) {
+      cellState = State.DEAD;
     }
   }
-  
+
   cellIs() {
     return cellState;
   }
-  
+
   var cellState;
 }
 
@@ -31,66 +33,67 @@ class Grid {
   Grid(this.xCount, this.yCount) {
     field = new Map();
   }
-  
+
   set(int x, int y, State state){
     field[pos(x,y)] = state;
   }
-  
+
   get(int x, int y){
     var state = field[pos(x,y)];
     return state != null ? state : State.DEAD;
   }
-  
+
   pos(x,y) {
     return '${(x+xCount)%xCount}:${(y+yCount)%yCount}';
   }
-  
+
   print() {
     var sb = new StringBuffer();
-    for (var x=0;x<xCount;x++) {
-      for (var y=0;y<yCount;y++) {
+    for (var x=0; x<xCount; x++) {
+      for (var y=0; y<yCount; y++) {
         sb.add(get(x,y).symbol);
       }
       sb.add("\n");
     }
     return sb.toString();
   }
-  
+
   final xCount, yCount;
   var field;
 }
 
 main() {
   group('rules', () {
-    test('lonely cell dies', () {
+    test('should let living but lonely cell die', () {
       var rule = new Rule(State.ALIVE);
       rule.reactToNeighbours(1);
       expect(rule.cellIs()).equals(State.DEAD);
     });
-    test('proper cell lives on', () {
+    test('should let proper cell live on', () {
       var rule = new Rule(State.ALIVE);
       rule.reactToNeighbours(2);
       expect(rule.cellIs()).equals(State.ALIVE);
     });
-    test('dead cell with three neighbours will be reborn', (){
+    test('should let dead cell with three neighbours be reborn', () {
       var rule = new Rule(State.DEAD);
       rule.reactToNeighbours(3);
       expect(rule.cellIs()).equals(State.ALIVE);
     });
-    test('cell with too many neighbours will die', (){
+    test('should let living cell with too many neighbours die', () {
       var rule = new Rule(State.ALIVE);
       rule.reactToNeighbours(4);
       expect(rule.cellIs()).equals(State.DEAD);
     });
   });
-  group('grid', (){
-    test('grid has state', (){
+
+  group('grid', () {
+    test('should have state', () {
       var grid = new Grid(1,1);
       expect(grid.get(0,0)).equals(State.DEAD);
       grid.set(0,0,State.ALIVE);
       expect(grid.get(0,0)).equals(State.ALIVE);
     });
-    test('grid has dimension', (){
+    test('should have dimension', () {
       var grid = new Grid(2,3);
       expect(grid.get(0,0)).equals(State.DEAD);
       grid.set(0,0,State.ALIVE);
@@ -99,19 +102,24 @@ main() {
       grid.set(1,2,State.ALIVE);
       expect(grid.get(1,2)).equals(State.ALIVE);
     });
-    test('grid is endless', (){
+    test('should be endless', () {
       var grid = new Grid(2,4);
       grid.set(2,4,State.ALIVE);
       expect(grid.get(0,0)).equals(State.ALIVE);
       grid.set(-1,-1,State.ALIVE);
       expect(grid.get(1,3)).equals(State.ALIVE);
     });
-    test('grid prints itself', (){
+    test('should print itself', () {
       var grid = new Grid(1,2);
       grid.set(0,1,State.ALIVE);
-      expect(grid.print()).equals("_X\n");
+      expect(grid.print()).equals(" #\n");
     });
-    //TODO count
-    //TODO next step
   });
+
+  group('game', () {
+    //TODO should count neigbours
+    //TODO should update grid in a step
+  });
+
+  //TODO run blinker in 3x3 for 3 iterations,
 }
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 RCDartGameOfLife
 ================
 
-Conways Game of Life in Dart
+[Conways Game of Life](http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) in [Dart](http://www.dartlang.org/)
+done during [Hackergarten Vienna](http://hackergarten.wikispaces.com/Vienna) for [Rosetta Code](http://rosettacode.org/wiki/Conway%27s_Game_of_Life)
 
 change the import path to your needs to run the code
