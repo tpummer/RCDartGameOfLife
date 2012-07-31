d5e7027f5376ffdd0f423321b433d2b670fafa35
Thomas Pummer <Thomas.Pummer@univie.ac.at>
2012-07-31 19:36:49 +0200

    Upgraded to Dartlang revison 1020

--- GoL.dart
+++ GoL.dart
@@ -1,4 +1,4 @@
-#import('../../../dart-sdk/lib/unittest/unittest.dart');
+#import('../../../../../privat/dart/dart-sdk/lib/unittest/unittest.dart');
 
 // see http://rosettacode.org/wiki/Conway%27s_Game_of_Life
 
@@ -67,52 +67,52 @@ main() {
     test('should let living but lonely cell die', () {
       var rule = new Rule(State.ALIVE);
       rule.reactToNeighbours(1);
-      expect(rule.cellIs()).equals(State.DEAD);
+      expect(rule.cellIs(), State.DEAD);
     });
     test('should let proper cell live on', () {
       var rule = new Rule(State.ALIVE);
       rule.reactToNeighbours(2);
-      expect(rule.cellIs()).equals(State.ALIVE);
+      expect(rule.cellIs(), State.ALIVE);
     });
     test('should let dead cell with three neighbours be reborn', () {
       var rule = new Rule(State.DEAD);
       rule.reactToNeighbours(3);
-      expect(rule.cellIs()).equals(State.ALIVE);
+      expect(rule.cellIs(), State.ALIVE);
     });
     test('should let living cell with too many neighbours die', () {
       var rule = new Rule(State.ALIVE);
       rule.reactToNeighbours(4);
-      expect(rule.cellIs()).equals(State.DEAD);
+      expect(rule.cellIs(), State.DEAD);
     });
   });
 
   group('grid', () {
     test('should have state', () {
       var grid = new Grid(1,1);
-      expect(grid.get(0,0)).equals(State.DEAD);
+      expect(grid.get(0,0), State.DEAD);
       grid.set(0,0,State.ALIVE);
-      expect(grid.get(0,0)).equals(State.ALIVE);
+      expect(grid.get(0,0), State.ALIVE);
     });
     test('should have dimension', () {
       var grid = new Grid(2,3);
-      expect(grid.get(0,0)).equals(State.DEAD);
+      expect(grid.get(0,0), State.DEAD);
       grid.set(0,0,State.ALIVE);
-      expect(grid.get(0,0)).equals(State.ALIVE);
-      expect(grid.get(1,2)).equals(State.DEAD);
+      expect(grid.get(0,0), State.ALIVE);
+      expect(grid.get(1,2), State.DEAD);
       grid.set(1,2,State.ALIVE);
-      expect(grid.get(1,2)).equals(State.ALIVE);
+      expect(grid.get(1,2), State.ALIVE);
     });
     test('should be endless', () {
       var grid = new Grid(2,4);
       grid.set(2,4,State.ALIVE);
-      expect(grid.get(0,0)).equals(State.ALIVE);
+      expect(grid.get(0,0), State.ALIVE);
       grid.set(-1,-1,State.ALIVE);
-      expect(grid.get(1,3)).equals(State.ALIVE);
+      expect(grid.get(1,3), State.ALIVE);
     });
     test('should print itself', () {
       var grid = new Grid(1,2);
       grid.set(0,1,State.ALIVE);
-      expect(grid.print()).equals(" #\n");
+      expect(grid.print(), " #\n");
     });
   });
 
